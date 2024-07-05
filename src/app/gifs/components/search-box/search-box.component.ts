319c3d93c2e0651a9166f783e9090031df51daac
Duvan <duvanriano2626@gmail.com>
2024-07-05 17:29:48 -0500

    Referencia al HTML con decoradores

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template:`
     <h5>Buscar</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs....">
+    <input type="text" class="form-control" placeholder="Buscar gifs...." (keyup.enter)="searchTag()" #txtTagImput>
   `,
 })
 export class SearchBoxComponent {
+
+  @ViewChild('txtTagImput')
+  public tagInput!: ElementRef <HTMLInputElement>;
+
+  public searchTag = (): void => {
+    const newTag = this.tagInput.nativeElement.value;
+    
+    console.log({newTag});
+  }
 
 }
