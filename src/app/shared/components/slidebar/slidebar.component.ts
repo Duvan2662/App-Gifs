cad4994465189252ba8713769fd1d0e4d9db17ac
Duvan <duvanriano2626@gmail.com>
2024-07-05 15:52:06 -0500

    Estructura del proyecto e inicio de la estructura del sliderBar

---
+++ src/app/shared/components/slidebar/slidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-slidebar',
+  templateUrl: './slidebar.component.html',
+  styleUrl: './slidebar.component.css'
+})
+export class SlidebarComponent {
+
+}
