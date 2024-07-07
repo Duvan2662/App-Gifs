4b829e17749a71c61319bd845389a1c03565837b
Duvan <duvanriano2626@gmail.com>
2024-07-07 14:50:27 -0500

    Animacion de carga de Gif se utiliza 1 libreria por CDN atento al index.html

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlidebarComponent } from './components/slidebar/slidebar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
-    SlidebarComponent
+    SlidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    SlidebarComponent
+    SlidebarComponent,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
